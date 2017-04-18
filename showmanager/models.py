bbf606a3a3c85ee3b87ca4ad976fa841ab54a508
Andrew Crozier <wacrozier@gmail.com>
2017-04-19 00:41:00 +0100

    Simplified model logic

--- showmanager/models.py
+++ showmanager/models.py
@@ -7,16 +7,9 @@ class League(db.Model):
     __tablename__ = 'leagues'
 
     id = db.Column(db.Integer, primary_key=True)
-
     name = db.Column(db.String)
-
     registration_start = db.Column(db.DateTime)
     registration_end = db.Column(db.DateTime)
-
-    last_entry = db.Column(db.DateTime, default=datetime.utcnow)
-    numbering_assigned = db.Column(db.DateTime)
-
-    scoring_rounds = db.Column(db.Integer)
 
     classes = db.relationship('Class', back_populates='league')
     rounds = db.relationship('Round', order_by='Round.date',
@@ -31,55 +24,41 @@ class League(db.Model):
         now = datetime.utcnow()
         return self.registration_start <= now <= self.registration_end
 
-    @property
-    def numbering_up_to_date(self):
-        if self.last_entry is None or self.numbering_assigned is None:
-            return False
-        return self.last_entry < self.numbering_assigned
-
-    def assign_numbering(self):
-        for i, entry in enumerate(self.entries):
-            entry.number = i + 1
-        self.numbering_assigned = datetime.utcnow()
-
 
 class Round(db.Model):
-    __tablename__ = 'rounds'
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, nullable=False)
-    league_id = db.Column(db.Integer, db.ForeignKey('leagues.id'))
-    league = db.relationship('League', back_populates='rounds')
 
-    courses = db.relationship('Course', back_populates='round')
-    # order_by='Course.clss.name')
+    __tablename__ = 'rounds'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+    league_id = db.Column(db.Integer, db.ForeignKey('leagues.id'),
+                          nullable=False)
+    league = db.relationship('League', back_populates='rounds')
 
     @property
     def number(self):
         sibling_ids = [round.id for round in self.league.rounds]
         return sibling_ids.index(self.id) + 1
 
-    @property
-    def name(self):
-        return 'Round {}'.format(self.number)
-
-    @property
-    def shortname(self):
-        return 'R{}'.format(self.number)
-
 
 class Class(db.Model):
+
     __tablename__ = 'classes'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
 
-    league_id = db.Column(db.Integer, db.ForeignKey('leagues.id'))
+    league_id = db.Column(db.Integer, db.ForeignKey('leagues.id'),
+                          nullable=False)
     league = db.relationship('League', back_populates='classes')
-
-    courses = db.relationship('Course', back_populates='clss')
 
 
 class Entry(db.Model):
+
     __tablename__ = 'entries'
+
     id = db.Column(db.Integer, primary_key=True)
     handler = db.Column(db.String)
     dog = db.Column(db.String)
@@ -89,10 +68,9 @@ class Entry(db.Model):
     collie = db.Column(db.Boolean)
     junior = db.Column(db.Boolean)
 
-    league_id = db.Column(db.Integer, db.ForeignKey('leagues.id'))
+    league_id = db.Column(db.Integer, db.ForeignKey('leagues.id'),
+                          nullable=False)
     league = db.relationship('League', back_populates='entries')
-
-    number = db.Column(db.Integer)
 
     scores = db.relationship('Score', back_populates='entry')
 
@@ -111,77 +89,58 @@ class Entry(db.Model):
 
 
 class Course(db.Model):
+
     __tablename__ = 'courses'
+
     id = db.Column(db.Integer, primary_key=True)
-
     round_id = db.Column(db.Integer, db.ForeignKey('rounds.id'))
-    round = db.relationship('Round', back_populates='courses')
-
     class_id = db.Column(db.Integer, db.ForeignKey('classes.id'))
-    clss = db.relationship('Class', back_populates='courses')
+
+    round = db.relationship('Round')
+    class_ = db.relationship('Class')
 
     time = db.Column(db.Float)
 
-    points_assigned = db.Column(db.DateTime)
-
     scores = db.relationship('Score', back_populates='course')
-
-    @property
-    def name(self):
-        return '{} {}'.format(self.clss.name, self.round.name)
-
-    @property
-    def points_up_to_date(self):
-
-        # Check if points have ever been assigned
-        if self.points_assigned is None:
-            return False
-
-        # Query for time of last updated score
-        most_recent = Score.query.filter(Score.course == self) \
-                                 .order_by(Score.modified.desc()) \
-                                 .first()
-
-        # Case when no scores have been applied
-        if most_recent is None:
-            # No points assignment to be done
-            return True
-
-        # Points are up to date as long as the last points assignment was later
-        # than or at the same time as the last modification of any score
-        return self.points_assigned >= most_recent.modified
 
     def update_points(self):
 
-        # Get the current time once to ensure all assigned times here are
-        # the same
-        now = datetime.now()
+        # Group scores by eliminated/clear round/with faults
+        clear_rounds = []
+        with_faults = []
+        eliminated = []
+        for score in self.scores:
+            if score.eliminated:
+                eliminated.append(score)
+            elif score.clear_round:
+                clear_rounds.append(score)
+            else:
+                with_faults.append(score)
+
+        # Sort clear rounds by time (smallest first)
+        clear_rounds = sorted(clear_rounds, key=lambda s: s.time)
+        # Sort rounds with faults by total faults (smallest first)
+        with_faults = sorted(clear_rounds, key=lambda s: s.total_faults)
 
         # Get total entrants in league
         n_entrants = Entry.query.filter_by(league=self.round.league) \
                                 .count()
 
-        # Assign points in order, with the best dog getting a number of points
-        # equal to the number of participants in the league, and each
-        # successive dog getting one point less
-        for i, s in enumerate(sorted(scores, key=lambda score: score.order)):
-
-            if s.noshow:
-                s.points = 0
-            elif s.eliminated:
-                s.points = 1
-            else:
-                s.points = n_entrants - i
-
-            s.modified = now
-
-        # Update points counter
-        self.points_assigned = now
+        # Assign points
+        # Non-eliminated rounds are assigned points in order, with the best
+        # score getting a number of points equal to the number of participants
+        # in the competition
+        for i, score in enumerate(clear_rounds + with_faults):
+            score.points = n_entrants - i
+        # Eliminated rounds get 1 point each
+        for score in eliminated:
+            score.points = 1
 
         db.session.commit()
 
 
 class Score(db.Model):
+
     __tablename__ = 'scores'
 
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'),
@@ -192,48 +151,27 @@ class Score(db.Model):
                          primary_key=True)
     entry = db.relationship('Entry', back_populates='scores')
 
-    faults = db.Column(db.Integer)
     time = db.Column(db.Float)
+    faults = db.Column(db.Integer)
     eliminated = db.Column(db.Boolean, default=False)
-
-    modified = db.Column(db.DateTime, default=datetime.now,
-                         onupdate=datetime.now)
     points = db.Column(db.Integer)
 
     @property
-    def noshow(self):
-        return self.time is None and not self.eliminated
-
-    @property
     def time_faults(self):
-        if self.eliminated or self.noshow:
+        if self.eliminated:
             return None
         time_over = self.time - self.course.time
         return time_over if time_over > 0. else 0.
 
     @property
     def total_faults(self):
-        if self.eliminated or self.noshow:
+        if self.eliminated:
             return None
         return self.faults + self.time_faults
 
     @property
     def clear_round(self):
         return self.total_faults == 0
-
-    @property
-    def order(self):
-        """
-        Score for ordering all participants by faults and time.
-        """
-        if self.noshow:
-            return -2e10
-        elif self.eliminated:
-            return -1e10
-        elif self.clear_round:
-            return self.time - self.course.time
-        else:
-            return self.total_faults
 
 
 def initialise():
@@ -249,8 +187,7 @@ def populate():
 
     league = League(name='Winter League 2016-17',
                     registration_start=datetime(2016, 6, 1),
-                    registration_end=datetime(2016, 8, 31),
-                    scoring_rounds=4)
+                    registration_end=datetime(2016, 8, 31))
     db.session.add(league)
 
     league_old = League(name='Winter League Old',
@@ -258,23 +195,24 @@ def populate():
                         registration_end=datetime(2015, 8, 31))
     db.session.add(league_old)
 
-    r1 = Round(date=date(2016, 9, 10), league=league)
-    r2 = Round(date=date(2016, 9, 17), league=league)
-    r3 = Round(date=date(2016, 9, 24), league=league)
-    r4 = Round(date=date(2016, 10, 1), league=league)
-    r5 = Round(date=date(2016, 10, 8), league=league)
-    r6 = Round(date=date(2016, 10, 15), league=league)
+    r1 = Round(name='Round 1', date=date(2016, 9, 10), league=league)
+    r2 = Round(name='Round 2', date=date(2016, 9, 17), league=league)
+    r3 = Round(name='Round 3', date=date(2016, 9, 24), league=league)
+    r4 = Round(name='Round 4', date=date(2016, 10, 1), league=league)
+    r5 = Round(name='Round 5', date=date(2016, 10, 8), league=league)
+    r6 = Round(name='Round 6', date=date(2016, 10, 15), league=league)
     db.session.add_all([r1, r2, r3, r4, r5, r6])
 
-    db.session.add(Round(date=date(2015, 12, 31), league=league_old))
+    db.session.add(Round(name='Round 1', date=date(2015, 12, 31),
+                         league=league_old))
 
     c1 = Class(name='Agility', league=league)
     c2 = Class(name='Jumping', league=league)
     db.session.add_all([c1, c2])
 
-    longname = 'really ' * 30
+    longname = 'Really ' + 'really ' * 30 + ' long name'
     entries = [Entry(handler='Some guy with a super duper long name',
-                     dog='Really ' + longname + 'long name',
+                     dog=longname,
                      size='S', grade=1, rescue=True, collie=False, junior=True,
                      league=league),
                Entry(handler='Andrew Crozier', dog='Caffrey',
@@ -291,21 +229,20 @@ def populate():
                      league=league)]
     db.session.add_all(entries)
 
-    course = Course(clss=c1, round=r1, time=29.)
+    course = Course(class_=c1, round=r1, time=29.)
     db.session.add(course)
 
-    from datetime import timedelta
-    tenmin = timedelta(seconds=60*10)
     for i, e in enumerate(entries[:3]):
-        time = datetime.now() - tenmin * i
         score = Score(course=course, entry=e, faults=i*5, time=30.+i*2.3,
-                      eliminated=False, created=time, modified=time)
+                      eliminated=False)
         db.session.add(score)
         db.session.commit()
     score = Score(course=course, entry=entries[3], eliminated=True)
     db.session.add(score)
 
     db.session.commit()
+
+    course.update_points()
 
 
 if __name__ == '__main__':
